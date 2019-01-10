978435c34aca8954275790ff790deccdeb4f4a14
Alex Peterson <alexpeterson@Alexs-MacBook-Pro.local>
2019-01-09 22:10:51 -0500

    Rev 2 based of reywood

--- keyboards/massdrop/alt/keymaps/apeterson/keymap.c
+++ keyboards/massdrop/alt/keymaps/apeterson/keymap.c
@@ -1,28 +1,30 @@
 #include QMK_KEYBOARD_H
+#include "rgb_matrix_user.h"
 
 enum alt_keycodes {
-    L_BRI = SAFE_RANGE, //LED Brightness Increase
-    L_BRD,              //LED Brightness Decrease
-    L_PTN,              //LED Pattern Select Next
-    L_PTP,              //LED Pattern Select Previous
-    L_PSI,              //LED Pattern Speed Increase
-    L_PSD,              //LED Pattern Speed Decrease
-    L_T_MD,             //LED Toggle Mode
-    L_T_ONF,            //LED Toggle On / Off
-    L_ON,               //LED On
-    L_OFF,              //LED Off
-    L_T_BR,             //LED Toggle Breath Effect
-    L_T_PTD,            //LED Toggle Scrolling Pattern Direction
-    U_T_AUTO,           //USB Extra Port Toggle Auto Detect / Always Active
-    U_T_AGCR,           //USB Toggle Automatic GCR control
-    DBG_TOG,            //DEBUG Toggle On / Off
-    DBG_MTRX,           //DEBUG Toggle Matrix Prints
-    DBG_KBD,            //DEBUG Toggle Keyboard Prints
-    DBG_MOU,            //DEBUG Toggle Mouse Prints
-    MD_BOOT,            //Restart into bootloader after hold timeout
+  L_BRI = SAFE_RANGE, //LED Brightness Increase
+  L_BRD,              //LED Brightness Decrease
+  L_PTN,              //LED Pattern Select Next
+  L_PTP,              //LED Pattern Select Previous
+  L_PSI,              //LED Pattern Speed Increase
+  L_PSD,              //LED Pattern Speed Decrease
+  L_T_MD,             //LED Toggle Mode
+  L_T_ONF,            //LED Toggle On / Off
+  L_ON,               //LED On
+  L_OFF,              //LED Off
+  L_T_BR,             //LED Toggle Breath Effect
+  L_T_PTD,            //LED Toggle Scrolling Pattern Direction
+  U_T_AUTO,           //USB Extra Port Toggle Auto Detect / Always Active
+  U_T_AGCR,           //USB Toggle Automatic GCR control
+  DBG_TOG,            //DEBUG Toggle On / Off
+  DBG_MTRX,           //DEBUG Toggle Matrix Prints
+  DBG_KBD,            //DEBUG Toggle Keyboard Prints
+  DBG_MOU,            //DEBUG Toggle Mouse Prints
+  MD_BOOT,            //Restart into bootloader after hold timeout
 };
 
 #define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
+#define ______ KC_TRNS
 
 keymap_config_t keymap_config;
 
@@ -69,125 +71,127 @@ void matrix_scan_user(void) {
 #define MODS_ALT  (keyboard_report->mods & MOD_BIT(KC_LALT) || keyboard_report->mods & MOD_BIT(KC_RALT))
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    static uint32_t key_timer;
+  static uint32_t key_timer;
 
-    switch (keycode) {
-        case L_BRI:
-            if (record->event.pressed) {
-                if (LED_GCR_STEP > LED_GCR_MAX - gcr_desired) gcr_desired = LED_GCR_MAX;
-                else gcr_desired += LED_GCR_STEP;
-                if (led_animation_breathing) gcr_breathe = gcr_desired;
-            }
-            return false;
-        case L_BRD:
-            if (record->event.pressed) {
-                if (LED_GCR_STEP > gcr_desired) gcr_desired = 0;
-                else gcr_desired -= LED_GCR_STEP;
-                if (led_animation_breathing) gcr_breathe = gcr_desired;
-            }
-            return false;
-        case L_PTN:
-            if (record->event.pressed) {
-                if (led_animation_id == led_setups_count - 1) led_animation_id = 0;
-                else led_animation_id++;
-            }
-            return false;
-        case L_PTP:
-            if (record->event.pressed) {
-                if (led_animation_id == 0) led_animation_id = led_setups_count - 1;
-                else led_animation_id--;
-            }
-            return false;
-        case L_PSI:
-            if (record->event.pressed) {
-                led_animation_speed += ANIMATION_SPEED_STEP;
-            }
-            return false;
-        case L_PSD:
-            if (record->event.pressed) {
-                led_animation_speed -= ANIMATION_SPEED_STEP;
-                if (led_animation_speed < 0) led_animation_speed = 0;
-            }
-            return false;
-        case L_T_MD:
-            if (record->event.pressed) {
-                led_lighting_mode++;
-                if (led_lighting_mode > LED_MODE_MAX_INDEX) led_lighting_mode = LED_MODE_NORMAL;
-            }
-            return false;
-        case L_T_ONF:
-            if (record->event.pressed) {
-                led_enabled = !led_enabled;
-                I2C3733_Control_Set(led_enabled);
-            }
-            return false;
-        case L_ON:
-            if (record->event.pressed) {
-                led_enabled = 1;
-                I2C3733_Control_Set(led_enabled);
-            }
-            return false;
-        case L_OFF:
-            if (record->event.pressed) {
-                led_enabled = 0;
-                I2C3733_Control_Set(led_enabled);
-            }
-            return false;
-        case L_T_BR:
-            if (record->event.pressed) {
-                led_animation_breathing = !led_animation_breathing;
-                if (led_animation_breathing) {
-                    gcr_breathe = gcr_desired;
-                    led_animation_breathe_cur = BREATHE_MIN_STEP;
-                    breathe_dir = 1;
-                }
-            }
-            return false;
-        case L_T_PTD:
-            if (record->event.pressed) {
-                led_animation_direction = !led_animation_direction;
-            }
-            return false;
-        case U_T_AUTO:
-            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
-            }
-            return false;
-        case U_T_AGCR:
-            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
-            }
-            return false;
-        case DBG_TOG:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
-            }
-            return false;
-        case DBG_MTRX:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
-            }
-            return false;
-        case DBG_KBD:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
-            }
-            return false;
-        case DBG_MOU:
-            if (record->event.pressed) {
-                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
-            }
-            return false;
-        case MD_BOOT:
-            if (record->event.pressed) {
-                key_timer = timer_read32();
-            } else {
-                if (timer_elapsed32(key_timer) >= 500) {
-                    reset_keyboard();
-                }
-            }
-            return false;
-        default:
-            return true; //Process all other keycodes normally
-    }
+  rgb_matrix_record_key_press(record);
+
+  switch (keycode) {
+    case L_BRI:
+      if (record->event.pressed) {
+        if (LED_GCR_STEP > LED_GCR_MAX - gcr_desired) gcr_desired = LED_GCR_MAX;
+        else gcr_desired += LED_GCR_STEP;
+        if (led_animation_breathing) gcr_breathe = gcr_desired;
+      }
+      return false;
+    case L_BRD:
+      if (record->event.pressed) {
+        if (LED_GCR_STEP > gcr_desired) gcr_desired = 0;
+        else gcr_desired -= LED_GCR_STEP;
+        if (led_animation_breathing) gcr_breathe = gcr_desired;
+      }
+      return false;
+    case L_PTN:
+      if (record->event.pressed) {
+        if (led_animation_id == led_setups_count - 1) led_animation_id = 0;
+        else led_animation_id++;
+      }
+      return false;
+    case L_PTP:
+      if (record->event.pressed) {
+        if (led_animation_id == 0) led_animation_id = led_setups_count - 1;
+        else led_animation_id--;
+      }
+      return false;
+    case L_PSI:
+      if (record->event.pressed) {
+        led_animation_speed += ANIMATION_SPEED_STEP;
+      }
+      return false;
+    case L_PSD:
+      if (record->event.pressed) {
+        led_animation_speed -= ANIMATION_SPEED_STEP;
+        if (led_animation_speed < 0) led_animation_speed = 0;
+      }
+      return false;
+    case L_T_MD:
+      if (record->event.pressed) {
+        led_lighting_mode++;
+        if (led_lighting_mode > LED_MODE_MAX_INDEX) led_lighting_mode = LED_MODE_NORMAL;
+      }
+      return false;
+    case L_T_ONF:
+      if (record->event.pressed) {
+        led_enabled = !led_enabled;
+        I2C3733_Control_Set(led_enabled);
+      }
+      return false;
+    case L_ON:
+      if (record->event.pressed) {
+        led_enabled = 1;
+        I2C3733_Control_Set(led_enabled);
+      }
+      return false;
+    case L_OFF:
+      if (record->event.pressed) {
+        led_enabled = 0;
+        I2C3733_Control_Set(led_enabled);
+      }
+      return false;
+    case L_T_BR:
+      if (record->event.pressed) {
+        led_animation_breathing = !led_animation_breathing;
+        if (led_animation_breathing) {
+          gcr_breathe = gcr_desired;
+          led_animation_breathe_cur = BREATHE_MIN_STEP;
+          breathe_dir = 1;
+        }
+      }
+      return false;
+    case L_T_PTD:
+      if (record->event.pressed) {
+        led_animation_direction = !led_animation_direction;
+      }
+      return false;
+    case U_T_AUTO:
+      if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+        TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
+      }
+      return false;
+    case U_T_AGCR:
+      if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+        TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+      }
+      return false;
+    case DBG_TOG:
+      if (record->event.pressed) {
+        TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+      }
+      return false;
+    case DBG_MTRX:
+      if (record->event.pressed) {
+        TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+      }
+      return false;
+    case DBG_KBD:
+      if (record->event.pressed) {
+        TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+      }
+      return false;
+    case DBG_MOU:
+      if (record->event.pressed) {
+        TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+      }
+      return false;
+    case MD_BOOT:
+      if (record->event.pressed) {
+        key_timer = timer_read32();
+      } else {
+        if (timer_elapsed32(key_timer) >= 500) {
+          reset_keyboard();
+        }
+      }
+      return false;
+    default:
+      return true; //Process all other keycodes normally
+  }
 }
